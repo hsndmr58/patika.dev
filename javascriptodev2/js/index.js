344a166f1790bc3633d6c995464297f33cc8d0ec
= <=>
2021-05-22 00:14:17 +0300

    Javascript 2.Ödev Finish

--- javascriptodev2/js/index.js
+++ javascriptodev2/js/index.js
@@ -1,7 +1,17 @@
 let userFormDOM = document.querySelector("#userForm")
 userFormDOM.addEventListener('submit', formHandler)
 const ulDOM=document.querySelector("#list")
-var values = [],keys = Object.keys(localStorage),loi = keys.length;
+
+keys =Object.keys(localStorage)
+let SListe=[]
+keys.forEach(value => {
+
+    SListe.push(value.slice(5))
+    
+    
+
+});
+SListe.sort(function(a, b){return a - b});
 function formHandler(event){
     event.preventDefault()
     const TASK=document.querySelector('#task')
@@ -9,11 +19,19 @@ function formHandler(event){
         $(".error").toast("show");
     }
     else{
+        let addItemControl=0;
+        if(SListe.length==0){
+            addItemControl=0
+        }
+        else{
+            addItemControl= parseInt(SListe[SListe.length-1])+1;
+        }
         newElement(TASK.value)
         $(".success").toast("show");
-        localStorage.setItem(`items${loi}`,JSON.stringify(loIDNOT))
+        localStorage.setItem(`items${addItemControl}`,JSON.stringify(loIDNOT))
         loIDNOT=[];
-        loi++;
+        location.reload(true)
+        
     }
 
     
@@ -21,22 +39,32 @@ function formHandler(event){
 }
 
 let loIDNOT
-let liID = loi;
+if(SListe.length==0){
+    liID=0;
+}
+else{
+    
+    liID=parseInt(SListe[SListe.length-1])+1;
+}
+
 const newElement = not =>{
     
     let eklenecek=`
     <li onclick="listechecked(${liID})" class="d-flex justify-content-between" id="Ls${liID}"><span class="mt-3 mb-2">${not}</span><button onclick="listedensil(${liID})" id="Lb${liID}" type="submit" class="sl-btn"></button></li>
     `
     loIDNOT={id:liID,nt:not}
-    liID++
+    
     ulDOM.innerHTML=ulDOM.innerHTML+eklenecek
+    
     
 }
 
 function listedensil(liID) {
     let LsID = document.querySelector(`#Ls${liID}`)
     LsID.parentNode.removeChild(LsID)
+    localStorage.removeItem(`items${liID}`)
     $(".warning").toast("show")
+
   
 }
 
@@ -58,22 +86,15 @@ function listechecked(liID){
 
     } 
 }
-let don=loi
 function listeyidiz(){
     
-    while ( don-- ) {
-        let userinfo = localStorage.getItem('items'+don)
-        userinfo=JSON.parse(userinfo)
-        
-        
+    SListe.forEach(saydir => {
+    let userinfo = localStorage.getItem('items'+saydir)
+    userinfo=JSON.parse(userinfo)
+    
     let eklenecek=`
     <li onclick="listechecked(${userinfo.id})" class="d-flex justify-content-between" id="Ls${userinfo.id}"><span class="mt-3 mb-2">${userinfo.nt}</span><button onclick="listedensil(${userinfo.id})" id="Lb${userinfo.id}" type="submit" class="sl-btn"></button></li>
     `
     ulDOM.innerHTML=ulDOM.innerHTML+eklenecek
-
-
-        values.push( localStorage.getItem(keys[don]) );
-    }
-
-    console.log(values)
+    }); 
 }
